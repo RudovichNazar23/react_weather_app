a22ec3eaedbb8c8541830e655b5f9b68ef27830e
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-02-10 23:31:12 +0100

    added logic for getting data from remote server using fetch api and showing this data at the page

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,14 +1,56 @@
+import { useState } from 'react';
+
 import SearchForm from './components/SearchForm';
 import MainContainer from './components/MainContainer';
 import Footer from './components/Footer';
 
 function App() {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState(
+    {
+      temp: "",
+      pressure: "",
+      humidity: "",
+      cityName: "",
+      windSpeed: "",
+      main: ""
+    }
+  );
+
+  const onChange = (event) => {
+    const value = event.target.value;
+    setCity(value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    if(!event.target.elements.cityName.value) return;
+
+    const apiId = process.env.REACT_APP_API_ID;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${apiId}`;
+
+
+    fetch(url).then(res => {
+      return res.json()
+    }).then(data => {
+      setData({
+        temp: data.main.temp,
+        pressure: data.main.pressure,
+        humidity: data.main.humidity,
+        cityName: data.name,
+        windSpeed: data.wind.speed,
+        main: data.weather[0].main
+      });
+    });
+  };
+
   return (
     <div className="container d-flex flex-column">
         <div className="container border border-dark rounded bg-light mt-5 p-3 d-flex flex-column">
-            <SearchForm />
+            <SearchForm onChange={onChange} city={city} onSubmit={onSubmit} />
             <hr />
-            <MainContainer />
+            <MainContainer city={data.cityName} temperature={data.temp} humidity={data.humidity} windSpeed={data.windSpeed} pressure={data.pressure} />
         </div>
         <div>
           <Footer imageSource={"sunny.png"} />
@@ -16,6 +58,6 @@ function App() {
     </div>
 
   );
-}
+};
 
 export default App;
--- weather_app/src/components/SearchForm.js
+++ weather_app/src/components/SearchForm.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
 
-function SearchForm(){
+
+function SearchForm({ onChange, onSubmit, city }){
     return (
-        <form className="container d-flex flex-row">
-            <input type="text" className="form-control" placeholder="Type the city"></input>
+        <form className="container d-flex flex-row" onSubmit={onSubmit}>
+            <input type="text" className="form-control" placeholder="Type the city" onChange={onChange} value={city} name="cityName"/>
             <button className="btn btn-info btn-rounded m-1">Search</button>
         </form>
     )
